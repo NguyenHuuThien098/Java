840bfdd1ccf049a7187ea81387a34e06b6bdc14f
Huu Thien <thiennguyenhuu3@gmail.com>
2025-02-22 14:37:23 +0700

    learning Java_abstraction

---
+++ Java_abstraction/src/Car.java
@@ -0,0 +1,11 @@
+
+public class Car extends Vehical {
+
+	@Override
+	void go() {
+		System.out.println("*The driver is driving the car*");
+	}
+
+	
+	
+}
---
+++ Java_abstraction/src/Vehical.java
@@ -0,0 +1,6 @@
+
+public abstract class Vehical {
+
+	abstract void go();
+	
+}
