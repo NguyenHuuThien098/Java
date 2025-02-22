e9b1075e44c0accf2308339967543a3464744217
Huu Thien <thiennguyenhuu3@gmail.com>
2025-02-22 15:35:59 +0700

    Java_constructors

---
+++ Java_constructors/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main (String[] args) {
+		// constructor = special method that is called when an object is instantiated (created)
+		
+		Human human = new Human("Thien", 22, 70);
+		
+		human.eat();
+		human.drink();
+		
+//		System.out.println(human.name);
+//		System.out.println(human.age);
+//		System.out.println(human.weight);
+	}
+}
