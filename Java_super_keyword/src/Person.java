4fe317ba13ddc8a1f7c76e7533cd0f9ccd6d2bdb
Huu Thien <thiennguyenhuu3@gmail.com>
2025-02-22 15:18:52 +0700

    Java_super_keyword

---
+++ Java_super_keyword/src/Person.java
@@ -0,0 +1,17 @@
+
+public class Person {
+
+	
+	String name;
+	int age;
+	
+	Person(String name, int age) {
+		this.age = age;
+		this.name = name;
+	}
+	
+	public String toString() {
+		return this.name + "\n" + this.age + "\n";
+	}
+	
+}
